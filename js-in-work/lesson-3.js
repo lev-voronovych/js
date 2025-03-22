8b37ef76a0e812a7f2505ec4471fb18334091438
lev-voronovych <vlev0760@gmail.com>
2025-03-22 18:10:04 +0200

    lesson-3

---
+++ js-in-work/lesson-3.js
@@ -0,0 +1,19 @@
+
+let result = 5 + 5 + '5';
+console.log(result); 
+console.log(typeof result); 
+
+let email = "your_email@example.com";
+console.log(email.includes('@'));
+console.log(email.length); 
+
+
+let word1 = "My";
+let word2 = "name";
+let word3 = "is";
+let fullName = `${word1} ${word2} ${word3} Viktor`;
+console.log(fullName);
+
+let userName = "Олександро";
+let payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
